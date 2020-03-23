8b9fbc775db5ed579abbd15da1e9914bda2a380f
github username <github email address>
2020-03-23 16:14:22 -0600

    added a bunch of things

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { userData, photo } from "../data.js";
+// import { userData, photo } from "../data.js";
 import { Card, Button, Image, Loader } from "semantic-ui-react";
 
 /*No need to code in here! These are merely presentational components.*/
@@ -8,8 +8,7 @@ export const IntroTab = ({ dog }) => (
   <Card fluid>
     <Card.Content textAlign="center">
       <h3>Hi, my name is {dog.name}</h3>
-      {/* <p>Am I Adoptable: {dog.status}</p> */}
-      <h2>Photo Title:</h2>
+      <p>Am I Adoptable: Yes</p>
       <Image src={dog.picture} />
     </Card.Content>
   </Card>
@@ -19,27 +18,39 @@ export const Bio = ({ dog }) => (
   <Card fluid>
     <Card.Content textAlign="center">
       <h5>A little about me: </h5>
-      <h2>{photo.title}</h2>
+      {/* <h2>{photo.title}</h2> */}
       <p>Breed: {dog.breed}</p>
       <p>{dog.bio}</p>
       <ul>
-        <li>Friendly with Kids: {(dog.friendly_w_kids == true) ? "Yes" : "No"}</li>
-        <li>Energetic: {(dog.energetic == true) ? "Yes" : "No"}</li>
-        <li>Fixed: {(dog.fixed == true) ? "Yes" : "No"}</li>
-        <li>Friendly with Cats: {(dog.cats == true) ? "Yes" : "No"}</li>
-        <li>Friendly with Dogs: {(dog.dogs == true) ? "Yes" : "No"}</li>
+        <li>
+          Friendly with Kids: {dog.friendly_w_kids === true ? "Yes" : "No"}
+        </li>
+        <li>Energetic: {dog.energetic === true ? "Yes" : "No"}</li>
+        <li>Fixed: {dog.fixed === true ? "Yes" : "No"}</li>
+        <li>Friendly with Cats: {dog.cats === true ? "Yes" : "No"}</li>
+        <li>Friendly with Dogs: {dog.dogs === true ? "Yes" : "No"}</li>
       </ul>
     </Card.Content>
   </Card>
 );
 
-export const Adoption = ({ dog }) => (
+export const Adoption = (props) => {
+  const { dog } = props;
+  const { changeAdoptionToggle } = props;
+  const { addDogToFavorites } = props;
+
+  function handleClick(){
+    changeAdoptionToggle()
+    addDogToFavorites(dog)
+  }
+  return (
   <Card fluid>
     <Card.Content textAlign="center">
-      {/* <h2>{photo.title}</h2> */}
-      {/* <Image src={photo.url}/> */}
-      <button>Adopt Me!</button>
-      <button>Add to Favorites!</button>
+      <Button animated="fade" onClick={handleClick}>
+        <Button.Content visible>Adopt Me!</Button.Content>
+        <Button.Content hidden>:)</Button.Content>
+      </Button>
     </Card.Content>
   </Card>
-);
+  )
+};
