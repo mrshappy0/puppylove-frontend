1bdacb928bdeca7c4e80a85999ff487ec7a3d72f
mrshappy0 <ajs11300@gmail.com>
2020-03-23 14:37:46 -0600

    added a bunch of things

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,35 +1,45 @@
-import React from 'react'
-import {userData, photo} from '../data.js'
-import {Card, Button, Image, Loader} from 'semantic-ui-react'
+import React from "react";
+import { userData, photo } from "../data.js";
+import { Card, Button, Image, Loader } from "semantic-ui-react";
 
 /*No need to code in here! These are merely presentational components.*/
 
-export const IntroTab = () => (
+export const IntroTab = ({ dog }) => (
   <Card fluid>
     <Card.Content textAlign="center">
-      <h3>Hi, my name is {userData.name}</h3>
+      <h3>Hi, my name is {dog.name}</h3>
+      {/* <p>Am I Adoptable: {dog.status}</p> */}
+      <h2>Photo Title:</h2>
+      <Image src={dog.picture} />
+    </Card.Content>
+  </Card>
+);
+
+export const Bio = ({ dog }) => (
+  <Card fluid>
+    <Card.Content textAlign="center">
       <h5>A little about me: </h5>
-      <p>{userData.description}</p>
-      <p>Favorite phrase: {userData.catchphrase}</p>
-      <p>Age: {userData.age}</p>
+      <h2>{photo.title}</h2>
+      <p>Breed: {dog.breed}</p>
+      <p>{dog.bio}</p>
+      <ul>
+        <li>Friendly with Kids: {(dog.friendly_w_kids == true) ? "Yes" : "No"}</li>
+        <li>Energetic: {(dog.energetic == true) ? "Yes" : "No"}</li>
+        <li>Fixed: {(dog.fixed == true) ? "Yes" : "No"}</li>
+        <li>Friendly with Cats: {(dog.cats == true) ? "Yes" : "No"}</li>
+        <li>Friendly with Dogs: {(dog.dogs == true) ? "Yes" : "No"}</li>
+      </ul>
     </Card.Content>
   </Card>
-)
+);
 
-export const Bio = () => (
+export const Adoption = ({ dog }) => (
   <Card fluid>
     <Card.Content textAlign="center">
-      <h2>{photo.title}</h2>
-      <Image src={photo.url}/>
+      {/* <h2>{photo.title}</h2> */}
+      {/* <Image src={photo.url}/> */}
+      <button>Adopt Me!</button>
+      <button>Add to Favorites!</button>
     </Card.Content>
   </Card>
-)
-
-export const Adoption = () => (
-  <Card fluid>
-  <Card.Content textAlign="center">
-    <h2>{photo.title}</h2>
-    <Image src={photo.url}/>
-  </Card.Content>
-</Card>
-)
+);
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -4,7 +4,13 @@ import { IntroTab, Bio, Adoption } from "../components/Tabs.js";
 
 class MainBox extends React.Component {
   state = {
-    selected: "IntroTab"
+    selected: "IntroTab",
+    dogs: [],
+    dog: {
+      name: "bob",
+      age: 19
+    },
+    dogCounter: 0
   };
 
   changeSelected = selected => {
@@ -13,19 +19,38 @@ class MainBox extends React.Component {
     });
   };
 
-  render() {
+  componentDidMount() {
+    fetch("http://localhost:3003/dogs")
+      .then(response => response.json())
+      .then(dogs => {
+        this.setState({
+          dogs: dogs
+        });
+        this.setState({
+          dog: this.state.dogs[this.state.dogCounter]
+        });
+      });
+  }
 
+  pickNextDog = () => {
+    this.setState({
+      dog: this.state.dogs[this.state.dogCounter + 1],
+      dogCounter: this.state.dogCounter + 1
+    });
+  };
+
+  render() {
     let details;
 
     switch (this.state.selected) {
       case "IntroTab":
-        details = <IntroTab />;
+        details = <IntroTab dog={this.state.dog} />;
         break;
       case "Bio":
-        details = <Bio />;
+        details = <Bio dog={this.state.dog} />;
         break;
       case "Adoption":
-        details = <Adoption />;
+        details = <Adoption dog={this.state.dog} />;
         break;
       default:
         details = null;
@@ -35,8 +60,13 @@ class MainBox extends React.Component {
     return (
       <div>
         <h2>Welcome, {this.props.user.username}!</h2>
-        <Menu changeSelected={this.changeSelected} selected={this.state.selected}/>
+
+        <Menu
+          changeSelected={this.changeSelected}
+          selected={this.state.selected}
+        />
         {details}
+        <button onClick={this.pickNextDog}>Swipe Me</button>
       </div>
     );
   }
